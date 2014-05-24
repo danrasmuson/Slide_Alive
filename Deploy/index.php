30f234a14e21d0bc37104e4e9bc3cd58e90c99cb
danielrasmuson <dan123911@gmail.com>
2014-05-24 15:16:09 -0500

    added noun list and splash page

---
+++ Deploy/index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>SlideAlive Studios</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+</head>
+<body>
+    <div id="title">
+        <div id="logo"><img src="img/logoPNG.png" alt=""></div>
+        <header>
+        slide &nbsp;&nbsp;&nbsp;&nbsp;alive <div id="studios">studios</div>
+        </header>
+    </div>
+
+</body>
+</html>
