5a6ef0d095ebeab6e7e385de253bac3db66abf6a
danielrasmuson <dan123911@gmail.com>
2014-05-24 21:29:16 -0500

    added sign in system

---
+++ Deploy/review.php
@@ -0,0 +1,110 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Create a SlideAlive Presentation</title>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="css\create.css"> -->
+    <!-- <script type="text/javascript" src="js/textArea.js"></script> -->
+    <script src="js/bootstrap.js"></script>
+    <link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="css\review.css">
+</head>
+
+<body>
+
+<div class="container">
+    <div id="signIn">
+        <div class="row">
+            <div class="col-md-3 col-md-offset-3" >
+                <form class="form-signin" role="form" _lpchecked="1">
+                    <h2 class="form-signin-heading">please sign in</h2>
+                    <input type="email" class="form-control" placeholder="email address" required="" autofocus="" autocomplete="off">
+                    <input type="password" class="form-control" placeholder="password" required="" autocomplete="off">
+                    <br>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">download</button>
+                </form>
+            </div>          
+            <div class="col-md-3">
+                <form class="form-signin" role="form" _lpchecked="1">
+                    <h2 class="form-signin-heading">please sign in</h2>
+                    <input type="name" class="form-control" placeholder="first name" required="" autofocus="" autocomplete="off">
+                    <input type="name" class="form-control" placeholder="last name" required="" autofocus="" autocomplete="off">
+                    <input type="email" class="form-control" placeholder="email address" required="" autofocus="" autocomplete="off">
+                    <input type="password" class="form-control" placeholder="password" required="" autocomplete="off">
+                    <br>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit">download</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <span class="slideTitle">change slide one</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <span class="sentance">I
+                <span class="highlight">love</span>
+                <span class="highlight">pizza</span>, its so delicious.</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <span>
+                <img src="img/pizza1.jpg" alt="pizza">
+            </span>
+        </div>
+        <div class="col-md-4">
+            <span>
+                <img src="img/pizza2.jpg" alt="pizza">
+            </span>
+        </div>
+        <div class="col-md-4">
+            <span>
+                <img src="img/pizza3.jpg" alt="pizza">
+            </span>
+        </div>
+    </div>
+
+    <br>
+    <br>
+    
+
+    <div class="row">
+        <div class="col-md-12">
+            <span class="slideTitle">change slide two</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <span class="sentance">Wow,
+                <span class="highlight">fish</span> are so cool.</span>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <span>
+                <img src="img/fish1.jpg" alt="fish">
+            </span>
+        </div>
+        <div class="col-md-4">
+            <span>
+                <img src="img/fish2.jpg" alt="fish">
+            </span>
+        </div>
+        <div class="col-md-4">
+            <span>
+                <img src="img/fish3.jpg" alt="fish">
+            </span>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+</html>
