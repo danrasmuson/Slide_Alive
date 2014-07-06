623acacaf0a23f29e90254c41eb9b88d0e363346
danielrasmuson <danielrasmuson@gmail.com>
2014-07-06 12:28:52 -0500

    missing a few files from Will's system. Grabed them and change structure.

--- Deploy/create.php
+++ Deploy/create.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="css\create.css">
     <script type="text/javascript" src="js/textArea.js"></script>
     <script src="js/bootstrap.js"></script>
+	<script src="js/bootstrap.js"></script>
+	<script src="http://fgnass.github.io/spin.js/spin.min.js"></script>
     <link href='http://fonts.googleapis.com/css?family=Roboto:100' rel='stylesheet' type='text/css'>
 </head>
 <body>
@@ -15,15 +17,34 @@
         <div class="col-md-8 col-md-offset-2">
             <table>
                 <tr>
-                    <td id="greyBackground" onkeyup="textAreaAdjust(this)">
+                    <div id="spinhere"></div><td id="greyBackground" onkeyup="textAreaAdjust(this)">
                         <div id="logo"><img src="img/logoPNG.png" alt=""></div>
                         <div id="nullBox"></div>
                     </td>
 
                     <td width="100%">
-                        <form class="form-inline" method="get" action="http://sa.lbsg.net/api.php">
-                            <textarea onkeyup="textAreaAdjust(this)" name="input" style="overflow:hidden; width: 100%;" placeholder="Enter Text for the Slidshow"></textarea>
-                            <input type="submit" value="done" class="button">
+                        <form class="form-inline" method="post" action="http://sa.lbsg.net/api.php">
+                            <textarea onkeyup="textAreaAdjust(this)" name="input" style="overflow:hidden; width: 100%;" placeholder="Enter some text here, and we'll turn it into a presentation."></textarea>
+                            <input type="submit" value="done" class="button" onclick="var opts = {
+								  lines: 13, // The number of lines to draw
+								  length: 4, // The length of each line
+								  width: 3, // The line thickness
+								  radius: 45, // The radius of the inner circle
+								  corners: 1, // Corner roundness (0..1)
+								  rotate: 0, // The rotation offset
+								  direction: 1, // 1: clockwise, -1: counterclockwise
+								  color: '#FFFFFF', // #rgb or #rrggbb or array of colors
+								  speed: 1, // Rounds per second
+								  trail: 48, // Afterglow percentage
+								  shadow: true, // Whether to render a shadow
+								  hwaccel: true, // Whether to use hardware acceleration
+								  className: 'spinner', // The CSS class to assign to the spinner
+								  zIndex: 2e9, // The z-index (defaults to 2000000000)
+								  top: '40%', // Top position relative to parent
+								  left: '8.3%' // Left position relative to parent
+								};
+								var target = document.getElementById('foo');
+								var spinner = new Spinner(opts).spin(spinhere);">
                         </form>
 
                     </td>
--- Deploy/include/flickr.php
+++ Deploy/include/flickr.php
@@ -6,7 +6,7 @@ class Flickr {
 	} 
  
 	public function search($query = null) { 
-		$search = 'http://flickr.com/services/rest/?method=flickr.photos.search&api_key=' . $this->apiKey . '&text=' . urlencode($query) . '&per_page=3&format=php_serial&sort=relevance';
+		$search = 'http://flickr.com/services/rest/?method=flickr.photos.search&api_key=' . $this->apiKey . '&text=' . urlencode($query) . '&per_page=1&format=php_serial&sort=relevance';
 		$result = file_get_contents($search); 
 		$final_result = unserialize($result);
 		return $final_result;
